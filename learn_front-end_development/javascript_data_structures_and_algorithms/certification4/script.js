5268444f7d7402d47d916b0fe411d8085c675e18
张正道 <zzd1645@gmail.com>
2024-04-15 10:52:33 +0800

    fix

--- learn_front-end_development/javascript_data_structures_and_algorithms/certification4/script.js
+++ learn_front-end_development/javascript_data_structures_and_algorithms/certification4/script.js
@@ -115,3 +115,49 @@ purchaseBtn.addEventListener("click", () => {
         cashRegister.init(cid).changeDue(cash, price);
     }
 })
+
+
+/*
+const unitMap = {
+  "PENNY": 0.01,
+  "NICKEL": 0.05,
+  "DIME": 0.1,
+  "QUARTER": 0.25,
+  "ONE": 1,
+  "FIVE": 5,
+  "TEN": 10,
+  "TWENTY": 20,
+  "ONE HUNDRED": 100,
+}
+
+function checkCashRegister(price, cash, cid) {
+  const totalCash = parseFloat(cid.reduce((total, item) => total += item[1], 0).toFixed(2));
+  const returnCash = parseFloat((cash - price).toFixed(2));
+  if (totalCash < returnCash) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  } else if (totalCash === returnCash) {
+    return { status: "CLOSED", change: cid };
+  } else {
+    const computed = computeReturn(returnCash, cid);
+    return { status: computed.length ? "OPEN" : "INSUFFICIENT_FUNDS", change: computed };
+  }
+}
+
+function computeReturn(cash, cid) {
+  let arr = [];
+  for (let i = cid.length - 1; i >= 0; i--) {
+    const unit = unitMap[cid[i][0]];
+    if (cash >= unit && cid[i][1]) {
+      const needCount = Math.floor(cash / unit);
+      const actualCash = needCount * unit > cid[i][1] ? cid[i][1] : needCount * unit;
+      arr.push([cid[i][0], actualCash]);
+      cash = parseFloat((cash - actualCash).toFixed(2));
+    }
+  }
+  return cash > 0 ? [] : arr;
+
+}
+
+const res = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(res)
+ */
